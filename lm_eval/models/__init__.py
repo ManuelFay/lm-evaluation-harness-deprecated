4899dc2bb20eed3d04675934e278449f92190597
ManuelFay <manuel.faysse@illuin.tech>
2024-03-22 17:25:14 +0100

    old commit version for archiving

---
+++ lm_eval/models/__init__.py
@@ -0,0 +1,9 @@
+from . import huggingface
+from . import openai_completions
+from . import textsynth
+from . import dummy
+from . import anthropic_llms
+from . import gguf
+
+
+# TODO: implement __all__
